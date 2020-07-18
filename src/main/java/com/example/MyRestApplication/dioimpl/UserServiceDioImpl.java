b691dcda045b7d32668c82b909c6309595a38d60
ravi kumar <91raviindia@gmail.com>
2020-07-18 16:19:54 +0530

    enable show sql

--- src/main/java/com/example/MyRestApplication/dioimpl/UserServiceDioImpl.java
+++ src/main/java/com/example/MyRestApplication/dioimpl/UserServiceDioImpl.java
@@ -1,8 +1,8 @@
 package com.example.MyRestApplication.dioimpl;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.example.MyRestApplication.dio.UserServiceDio;
@@ -11,9 +11,8 @@ import com.example.MyRestApplication.entity.User;
 @Repository
 public class UserServiceDioImpl implements UserServiceDio {
 
-	@Autowired
+	@PersistenceContext
 	private EntityManager entityManager;
-
 
 	@Override
 	public int saveUser(User user) {
